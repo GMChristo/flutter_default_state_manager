cf18aea6118acc2aedeebea8e560f9ee62c479a3
GMChristo <gabrielchristo@hotmail.com>
2024-01-12 18:44:22 -0300

    adicionado exemplo de utilização de setState para gerencia de estado

--- lib/template.dart
+++ lib/template.dart
@@ -1,16 +1,15 @@
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_default_state_manager/widgets/imc_gauge_range.dart';
-import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:flutter_default_state_manager/widgets/imc_gauge.dart';
 
-class ImcSetstatePage extends StatefulWidget {
-  const ImcSetstatePage({super.key});
+class Template extends StatefulWidget {
+  const Template({super.key});
 
   @override
-  State<ImcSetstatePage> createState() => _ImcSetstatePageState();
+  State<Template> createState() => _TemplateState();
 }
 
-class _ImcSetstatePageState extends State<ImcSetstatePage> {
+class _TemplateState extends State<Template> {
   final pesoEC = TextEditingController();
   final alturaEC = TextEditingController();
 
@@ -32,55 +31,7 @@ class _ImcSetstatePageState extends State<ImcSetstatePage> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              SfRadialGauge(
-                axes: [
-                  RadialAxis(
-                    showLabels: false,
-                    showAxisLine: false,
-                    showTicks: false,
-                    minimum: 12.5,
-                    maximum: 47.9,
-                    ranges: [
-                      ImcGaugeRange(
-                        color: Colors.blue,
-                        start: 12.5,
-                        end: 18.5,
-                        label: 'MAGREZA',
-                      ),
-                      ImcGaugeRange(
-                        color: Colors.green,
-                        start: 18.5,
-                        end: 24.5,
-                        label: 'NORMAL',
-                      ),
-                      ImcGaugeRange(
-                        color: Colors.yellow[600]!,
-                        start: 24.5,
-                        end: 29.9,
-                        label: 'SOBREPESO',
-                      ),
-                      ImcGaugeRange(
-                        color: Colors.red[500]!,
-                        start: 29.9,
-                        end: 39.9,
-                        label: 'OBESIDADE',
-                      ),
-                      ImcGaugeRange(
-                        color: Colors.red[900]!,
-                        start: 39.9,
-                        end: 47.9,
-                        label: 'OBESIDADE GRAVE',
-                      ),
-                    ],
-                    pointers: const [
-                      NeedlePointer(
-                        value: 15,
-                        enableAnimation: true,
-                      )
-                    ],
-                  ),
-                ],
-              ),
+              const ImcGauge(imc: 0),
               const SizedBox(
                 height: 20,
               ),
@@ -113,7 +64,7 @@ class _ImcSetstatePageState extends State<ImcSetstatePage> {
               const SizedBox(
                 height: 20,
               ),
-              ElevatedButton(onPressed: () {}, child: Text('Calcular IMC'))
+              ElevatedButton(onPressed: () {}, child: const Text('Calcular IMC'))
             ],
           ),
         ),
